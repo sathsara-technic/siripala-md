25215121b09e3e0dc073f07b5725188719531af1
Sathsara Modz <145262720+sathsara-technic@users.noreply.github.com>
2024-11-17 12:24:09 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "url",
+ALIVE_MSG: process.env.ALIVE_MSG || "suwahasboy@gmail.com",
+};
